342ef4dc8f7c98134777c1d83f36314ce7c72283
meyang1 <52054330+meyang1@users.noreply.github.com>
2019-07-12 21:51:59 -0500

    Update factory.cpp

--- factory.cpp
+++ factory.cpp
@@ -1,3 +1,6 @@
+//Milk Factory
+//USACO 2019 US Open Contest
+
 #include <cstdio>
 #include <cstdlib>
 
@@ -10,15 +13,15 @@ int main(){
         fscanf(In, "%d %d", &A[i], &B[i]);
     }
     fclose(In);
-
+    //----------------------------------------
     for (int i_lines = 1; i_lines <= N-1; i_lines++){
         int lines = i_lines;
         for (int i_stations = 1; i_stations <= N; i_stations++){
             int stations = i_stations;
             int count;
-            count++;
+            count++; //number of lines used to find equal station
             if (B[lines] == stations){
-                //for each of the values in array b as a possible station, find station w/ total of N-1
+                //finds station equal to line
                 break;
             }
         }   
@@ -27,11 +30,19 @@ int main(){
        int afterlines = i_afterlines;
        if (B[afterlines] == stations){
           int second_count;
-          second_count++;
+          second_count++; //counts number of total stations equal to station
        }    
    }
     
    if (second_count == N-1){
-       
+       printf("Station : %d", second_count); //If specified station is available to all except own, then answer
+   }else{
+       second_count = -1;   
+   }
+    
+   //----------------------------------------
+    FILE *Out =fopen("factory.out", "w");
+    fprintf(Out,"%d\n",  second_count);
+    fclose(Out);
 }
     
