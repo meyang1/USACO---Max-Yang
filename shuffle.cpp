1f4c4a74506b9ba71debcb631183d9e5e61bd2fe
meyang1 <52054330+meyang1@users.noreply.github.com>
2019-07-26 14:42:28 -0500

    Create shuffle.cpp

---
+++ shuffle.cpp
@@ -0,0 +1,50 @@
+//Bovine Shuffle
+//By: Max Yang
+//Time: 1 hour and 30 minutes
+/*
+The problem took some time to understand, but I think I got the main parts 
+of it. I mostly spent time trying to implement maps into the programming, 
+which I still didn't really understand completely after using them. 
+*/
+int main(){
+    std::map<int, int> CowId;
+    std::map<int,int>::iterator it;
+
+    FILE * In = fopen("shuffle.in", "r");
+
+    int cows;
+    fscanf(In, "%d", &cows);
+
+    //Position after 3 shuffles
+    int pos[cows+1];
+    for(int i=0; i<=cows; i++){
+            fscanf(In, "%d", &pos[i]);
+    }
+
+    //Cow Ids
+    int id[cows+1];
+    for(int j=0; j<=cows; j++){
+            fscanf(In, "%d", &id[j]);
+    }
+
+    fclose(In);
+
+    //Input into map
+    for(int k=0; k<=cows; k++){
+            int m;
+            pos[k] = m;
+            CowId.insert(std::pair<int, int> (m,id[k]));
+    }
+
+    FILE * Out = fopen("shuffle.out", "w");
+
+    for(int m=1; m<=cows; m++){
+           int ans = CowId.find(m)->second;
+           printf("%d\n", ans);
+
+           fprintf(Out, "%d\n", ans);
+    }
+
+
+    fclose(Out);
+}
